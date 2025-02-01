9b171508574e2869531247ba2f40bf4430ff24a0
Airof <fmhmdairof@gmail.com>
2025-02-02 02:23:35 +0330

    add ALU
    Add ALU (arithmatic , logic and shift unit) to the project

--- Common-Bus-System/ALU/ALU.vhd
+++ Common-Bus-System/ALU/ALU.vhd
@@ -2,26 +2,50 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 -- use IEEE.numeric_std.all;
 
+
 entity ALU is
     port (
+        S : in std_logic_vector(3 downto 0);
+        cin: std_logic;
         A, B: in std_logic_vector(15 downto 0);
-        sel: in std_logic_vector(1 downto 0);
-        cin: in std_logic;
-
-        cout: out std_logic;
-        D: out std_logic_vector(15 downto 0)
+        F: out std_logic_vector(15 downto 0);
+        E: out std_logic
     );
 end entity ALU;
 
 
+
 architecture Behavorial of ALU is
 
-    component FullAdder is
+    
+    component Arithmatic is
         port (
-            cin, A, B : in std_logic;
-            cout, S : out std_logic 
+            A, B: in std_logic_vector(15 downto 0);
+            sel: in std_logic_vector(1 downto 0);
+            cin: in std_logic;
+            
+            cout: out std_logic;
+            D: out std_logic_vector(15 downto 0)
+            );
+        end component Arithmatic;
+    
+
+    component LOGIC is
+        port (
+            Sel: in std_logic_vector(1 downto 0);
+            A, B: in std_logic_vector(15 downto 0);
+            E: out std_logic_vector(15 downto 0)
         );
-    end component;
+    end component LOGIC;
+
+
+    component ShiftLR is
+        port (
+            Shift_dir: in std_logic;
+            A: in std_logic_vector(15 downto 0);
+            H: out std_logic_vector(15 downto 0)
+        );
+    end component ShiftLR;
 
     component MUX4to1 is
         port (
@@ -30,40 +54,54 @@ architecture Behavorial of ALU is
             q : out std_logic
         );
     end component;
+        
 
-    signal NOTB: std_logic_vector(15 downto 0);
+    signal Arith_val: std_logic_vector(15 downto 0);
+    signal logic_val: std_logic_vector(15 downto 0);
+    signal shift_val_L: std_logic_vector(15 downto 0);
+    signal shift_val_R: std_logic_vector(15 downto 0);
 
-    signal mux_output: std_logic_vector(15 downto 0);
-    signal carry: std_logic_vector(16 downto 0);
 begin
     
-
-    NOTB <= NOT B;
-    carry(0) <= cin;
-    cout <= carry(16);
-
-    GEN_MUX: for i in 0 to 15 generate
-        MUX: MUX4to1 port map(
-            input(0) => B(i),
-            input(1) => NOTB(i),
-            input(2) => '0',
-            input(3) => '1',
-            sel => sel,
-            q => mux_output(i)
-        );
-    end generate Gen_mux;
-
-
-    GEN_FA: for i in 0 to 15 generate
-    FA: FullAdder port map(
-        cin => carry(i),
-        A => A(i),
-        B => mux_output(i),
-        cout => carry(i+1),
-        S => D(i)
+    Arithmatic_block: Arithmatic port map(
+        A => A,
+        B => B,
+        sel => S(1 downto 0),
+        cin => cin,
+        cout => E,
+        D => Arith_val
     );
-    end generate Gen_FA;
+
+    Logic_block: LOGIC port map(
+        Sel => S(1 downto 0),
+        A => A,
+        B=> B,
+        E=> logic_val
+    );
+
+    Shift_Right_block: ShiftLR port map(
+        Shift_dir => '0',
+        A => A,
+        H => shift_val_R
+    );
 
 
+    Shift_Left_block: ShiftLR port map(
+        Shift_dir => '1',
+        A => A,
+        H => shift_val_L
+    );
 
+    FINAL_VALUE_GEN: for i in 0 to 15 generate  
+        Final_value: MUX4to1 port map(
+            input(0) => Arith_val(i),
+            input(1) => logic_val(i),
+            input(2) => shift_val_R(i),
+            input(3) => shift_val_L(i),
+            sel => S(3 downto 2),
+            q => F(i)
+        );
+    end generate;
+
+    
 end architecture Behavorial;
--- Common-Bus-System/waves/ALU.do
+++ Common-Bus-System/waves/ALU.do
@@ -1,14 +1,18 @@
-vcom ALU/FullAdder.vhd
+vcom ALU/MUX2to1.vhd
+vcom ALU/Shift-register.vhd
 vcom ALU/MUX4to1.vhd
-vcom ALU/ALU.vhd
+vcom ALU/Logic-unitX1.vhd
+vcom ALU/Logic-unit.vhd
+vcom ALU/FullAdder.vhd
+vcom ALU/Arithmatic-unit.vhd
+
 vsim work.ALU
-add wave *
+add wave *  
 
+force A 1111111100000000 0ns, 0011110111011100 100ns, 0011101010011100 200ns, 0000001111111111 300ns -repeat 400ns
+force B 0000000011111111 0ns, 0000001111111111 200ns -repeat 400ns
 
-force A 1111000011110000
-force B 1111000000001111
+force cin 0 0ns, 1 50ns -repeat 100ns
+force ALU/S 0000 0ns, 0001 20ns, 0010 40ns, 0011 60ns, 0100 80ns, 0101 100ns, 0110 120ns, 0111 140ns, 1000 160ns, 1001 180ns, 1010 200ns, 1011 220ns, 1100 240ns, 1101 260ns, 1110 280ns, 1111 300ns -repeat 320ns
 
-force sel 00 0, 01 20, 10 40 , 11 60 -repeat 80
-force cin 0 0, 1 10 -repeat 20
-
-run 80
+run 4000ns
