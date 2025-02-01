e5fe3c66b4ebf9781d1751905e7b4ba48a41252b
amirarsalan7 <balaghiamirarslan901@gmail.com>
2025-02-02 00:37:11 +0330

    fix

--- Common-Bus-System/RAM/RandomAccessmemory.vhd
+++ Common-Bus-System/RAM/RandomAccessmemory.vhd
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 -- use IEEE.numeric_std.all;
 
----;osivnlisul vbasfuvbukfdv
 entity RandomAccessMemory is 
     port(
         addr : in std_logic_vector(11 downto 0);
